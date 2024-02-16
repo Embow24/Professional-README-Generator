03b8aed42f0c00d97ac1e89ce8efc1de98ae0eb8
Emily Kent <kentembowemily@gmail.com>
2024-02-16 19:32:21 +0000

    Prompt questions added to index.js file

--- index.js
+++ index.js
@@ -5,7 +5,51 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    { 
+    type:'input',
+    Question:'What is your GitHub user name?',
+    name: 'GitHub',
+},
+{ 
+    type:'email',
+    Question:'What is your email address?',
+    name: 'Email',
+},
+{ 
+    type:'input',
+    Question:'What is the title of your project?',
+    name: 'Title',
+},
+{ 
+    type:'input',
+    Question:'How would you describe your project?',
+    name: 'Description',
+},
+{ 
+    type:'input',
+    Question:'Which license will you use?',
+    name: 'License',
+},
+{ 
+    type:'input',
+    Question:'How do you install your project?',
+    name: 'Installation',
+},
+{ 
+    type:'input',
+    Question:'How do you use your project?',
+    name: 'Usage',
+},
+{ 
+    type:'input',
+    Question:'How can your project be contributed to?',
+    name: 'Contribute',
+},
+{ 
+    type:'input',
+    Question:'Which tests need to be run',
+    name: 'Tests',
+},
 ];
 
 // function to write README file
