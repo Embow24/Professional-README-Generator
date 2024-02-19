5ecd36a2622ade0d59addd8a1a4b48ed6b19b212
Emily Kent <kentembowemily@gmail.com>
2024-02-19 20:47:28 +0000

    Updated questions and license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,42 +3,37 @@ function createLicenseBadge(license) {
 switch (license) {
   case "MIT":
    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-  case "Apache":
+   case "Apache":
    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-  case "Eclipse":
+   case "Eclipse":
    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
-  case "Mozilla":
+   case "Mozilla":
    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-  case "None":
-    return ""
-  default:
-    return ""
+   case "None":
+   return ""
+    default:
+   return ""
+  }
 }
-}
-
-// function to display license badge information
-// function licenseBadgeDetails(license) {
-
-// }
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-  let badge = createLicenseBadge(data.License)
   return `# ${data.Title}
-  ${badge}
+  ${createLicenseBadge(data.License)}
 
   ## Description
   ${data.Description}
 
   ## Table of Contents:
-   *[License](#License)
-   *[Installation](#Installation)
-   *[Usage](#Usage)
-   *[Contribute](#Contribute)
-   *[Tests](#Tests)
-   *[Questions](#Questions)
+   * [License](#License)
+   * [Installation](#Installation)
+   * [Usage](#Usage)
+   * [Contribute](#Contribute)
+   * [Tests](#Tests)
+   * [Questions](#Questions)
 
   ## License
+  ${data.License}
 
   ## Installation
   ${data.Installation}
@@ -53,8 +48,8 @@ function generateMarkdown(data) {
   ${data.Tests}
 
   ## Questions
-  [Click to access GitHub](https://www.github.com/${data.GitHub})
-  [Send me an email at this address:]${data.Email}
+  * [Click to access GitHub](https://www.github.com/${data.GitHub})
+  * If you have any questions send me an email at this address: ${data.Email}
 `;
 }
 
